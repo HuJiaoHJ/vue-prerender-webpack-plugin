a87b59159334c8e8fd835aa24041a4c57abae8a8
hujiao <hujiao@meituan.com>
2018-01-30 20:18:49 +0800

    压缩build js文件

--- example/vue-prerender/webpack/webpack.prod.config.js
+++ example/vue-prerender/webpack/webpack.prod.config.js
@@ -1,10 +1,22 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const base = require('./webpack.base.config');
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 
 const prod = {
     plugins: [
         new webpack.optimize.AggressiveMergingPlugin(),
+        new ParallelUglifyPlugin({
+            cacheDir: '.cache/',
+            uglifyJS:{
+                output: {
+                    comments: false
+                },
+                compress: {
+                    warnings: false
+                },
+            },
+        }),
     ],
     stats: {
         children: false,
